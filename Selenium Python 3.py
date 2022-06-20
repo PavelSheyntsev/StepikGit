03cd69923b217ed3326fa25a4d044b453e89e477
Pavel <pavel-sheincev@mail.ru>
2022-06-20 19:20:46 +0300

    finished

--- Selenium Python 3.py
+++ Selenium Python 3.py
@@ -7,11 +7,21 @@ try:
     browser.maximize_window()
     browser.get("https://parsinger.ru/selenium/3/3.html")
 
-    #каждое число
-    a = browser.find_elements(By.TAG_NAME, "p")
-    a = [int(x.text) for x in browser.find_elements(By.TAG_NAME, "p")]
-    print(sum(a))
+    def test1():
+        #сумма каждое число
+        a = [int(x.text) for x in browser.find_elements(By.TAG_NAME, "p")]
+        print(sum(a))
+        assert a == int("450384194300")
 
+    def test2():
+        #сумма каждое второе число
+        a = [int(x.text) for x in browser.find_elements(By.XPATH, "//div[@class='text']/p[2]")]
+        print(sum(a))
+        assert a == int("149494128600")
+
+    if __name__ == "__main__":
+        test1()
+        test2()
 
 finally:
     time.sleep(1)
